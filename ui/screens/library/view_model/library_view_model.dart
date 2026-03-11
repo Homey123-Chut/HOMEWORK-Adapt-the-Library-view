118ad68ccb60bffe65f1dfc7d9f450393ce42162
Homey123-Chut <homey.chut@student.cadt.edu.kh>
2026-03-11 22:39:45 +0700

    Handle an AsyncValue for the list of songs

--- ui/screens/library/view_model/library_view_model.dart
+++ ui/screens/library/view_model/library_view_model.dart
@@ -3,10 +3,13 @@ import '../../../../data/repositories/songs/song_repository.dart';
 import '../../../states/player_state.dart';
 import '../../../../model/songs/song.dart';
 
+enum AsyncState { loading, success, error }
+
 class LibraryViewModel extends ChangeNotifier {
   final SongRepository songRepository;
   final PlayerState playerState;
   List<Song>? _songs;
+  AsyncState state = AsyncState.loading;
 
   LibraryViewModel({required this.songRepository, required this.playerState}) {
     playerState.addListener(notifyListeners);
@@ -24,12 +27,20 @@ class LibraryViewModel extends ChangeNotifier {
   }
 
   void _init() async {
+    state = AsyncState.loading;
     // 1 - Fetch songs
-    _songs = await songRepository.fetchSongs();
+    try {
+     _songs = await songRepository.fetchSongs();
+      state = AsyncState.success;
+    } catch (e) {
+      // 5. Catch error
+      state = AsyncState.error;
+    }
 
     // 2 - notify listeners
     notifyListeners();
   }
+  
 
   bool isSongPlaying(Song song) => playerState.currentSong == song;
 
